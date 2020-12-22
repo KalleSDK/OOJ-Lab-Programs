ee323de24d4aeb167b9b47d541e59da16a534620
fractalfist <sravan.dhira@gmail.com>
2020-12-22 10:52:22 +0530

    22/12/2020

--- Week-13/Prog1.java
+++ Week-13/Prog1.java
@@ -2,11 +2,32 @@ import java.awt.*;
 import java.awt.event.*;
 
 
+
+class MyDialog extends  Dialog implements ActionListener{
+    Prog1 p;
+    MyDialog(Frame parent, String title){
+        super(parent, title, false);
+        setLayout(new FlowLayout());
+        setSize(300,150);
+        p = (Prog1)parent;
+        Button b = new Button("OK");
+        Label l = new Label("Error: " + p.error);
+        add(l);
+        add(b);
+        b.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent ae){
+        dispose();
+    }
+}
+
+
 public class Prog1 extends Frame implements ActionListener{
     TextField Num1, Num2;
     Button divide = new Button("Divide");
     float res = 0;
-    String error = "";
+    public String error = "";
     public Prog1(){
         setLayout(new FlowLayout());
         Num1 = new TextField(1);
@@ -27,23 +48,32 @@ public class Prog1 extends Frame implements ActionListener{
             try{
                 int n1 = Integer.parseInt(Num1.getText());
                 int n2 = Integer.parseInt(Num2.getText());
-                if(n2 <= 0)
+                if(n2 <= 0){
                     throw new ArithmeticException("Error");
+                }
                 res = (float)n1/n2;
+                repaint();
             }catch(NumberFormatException exception){
+                System.out.println(exception);
                 res = 0;
                 error = "Entered number is not an integer.";
+                repaint();
             }catch(ArithmeticException exception){
+                System.out.println(exception);
                 res = 0;
                 error = "You tried to divide by zero.";
+                repaint();
             }
-            repaint();
+            if(res == 0){
+                System.out.println(this.error);
+                MyDialog d = new MyDialog(this, "Error");
+                d.setVisible(true);
+            }
         }
     }
 
     public void paint(Graphics g){
         g.drawString("Result: " + String.valueOf(res), 20, 100);
-        g.drawString("Error: " + error, 20, 150);
     }
 
     public static void main(String[] args) {
